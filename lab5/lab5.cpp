b423cfa85d81e592ba27f848f11aaf2cf4905d3c
Splashy34 <164371494+Splashy34@users.noreply.github.com>
2024-05-22 11:25:28 +0300

    Add files via upload

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -110,5 +110,69 @@ string* HashSet::ToArray()
 
 int main() 
 {
+	HashSet hashSet;
+	int choice;
+	string value;
+	do
+	{
+		cout << "1. Add an element." << endl;
+		cout << "2. Remove an element." << endl;
+		cout << "3. Check if an element is in the hash set." << endl;
+		cout << "4. Size of the hash set." << endl;
+		cout << "5. Clear the hash set." << endl;
+		cout << "6. Show all elements." << endl;
+		cout << "0. Exit." << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			cout << "Enter the element you want to add: " << endl;
+			cin >> value;
+			hashSet.add(value);
+			break;
+		case 2:
+			cout << "Enter the element to remove: " << endl;
+			cin >> value;
+			hashSet.remove(value);
+			break;
+		case 3:
+			cout << "Enter the element to check: " << endl;
+			cin >> value;
+			if (hashSet.contains(value))
+			{
+				cout << "The element is in the hash set." << endl;
+			}
+			else
+			{
+				cout << "The element is not in the hash set." << endl;
+			}
+			break;
+		case 4:
+			cout << "The size of the hash set is: " << hashSet.count() << endl;
+			break;
+		case 5:
+			hashSet.clear();
+			cout << "The hash set is cleared." << endl;
+			break;
+		case 6:
+		{
+			string* elements = hashSet.ToArray();
+			int count = hashSet.count();
+			cout << "Elements in the hash set: ";
+			for (int i = 0; i < count; ++i)
+			{
+				cout << elements[i] << " ";
+			}
+			cout << endl;
+			delete[] elements;
+			break;
+		}
+		case 0:
+			break;
+		default:
+			cout << "Unknown command. Try again." << endl;
+			break;
+		}
+	} while (choice != 0);
 	return 0;
 }
